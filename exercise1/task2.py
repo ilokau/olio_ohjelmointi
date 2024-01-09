3c5685ad70595086938525496a0f90c1f7aea269
Ilona Kauppila <123626072+ilokau@users.noreply.github.com>
2024-01-09 11:19:03 +0200

    Task 2

---
+++ exercise1/task2.py
@@ -0,0 +1,20 @@
+import random
+
+number_list = []
+string_list = []
+
+while len(number_list) < 10:
+    number = input("Please type a number:")
+    number_list.append(number)
+
+while len(string_list) < 10:
+    string = input("Please type a word:")
+    string_list.append(string)
+
+print("Adding random numbers to numbers list.")
+for i in range(0, 10):
+    number = random.randint(1, 100)
+    number_list[i] = number
+
+print(number_list)
+print(string_list)
