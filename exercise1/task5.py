eba4d2530f8f058aed0431e7c6adc6be7e9c409c
Ilona Kauppila <123626072+ilokau@users.noreply.github.com>
2024-01-17 12:35:57 +0200

    task 5

---
+++ exercise1/task5.py
@@ -0,0 +1,35 @@
+negnum_list = []
+even_count = 0
+
+
+def countEven(number):
+    global even_count
+    even_count += 1
+
+
+def isNeg(number, negnum_list):
+    negnum_list.append(number)
+
+
+def isZero():
+    if len(negnum_list) > 0:
+        print("Negative numbers entered:")
+        print(negnum_list)
+        print("The number of even numbers:", even_count)
+    else:
+        print("No negative numbers were entered.")
+        print("The number of even numbers:", even_count)
+
+
+while True:
+    number = int(input("Please type a number:"))
+
+    if number < 0:
+        isNeg(number, negnum_list)
+
+    if number == 0:
+        isZero()
+        break
+
+    if number % 2 == 0:
+        countEven(number)
